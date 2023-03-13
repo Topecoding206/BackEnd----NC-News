7186ba88505a78687f68c81eb416d5f34069dbb9
Topecoding206 <topecoding@gmail.com>
2023-03-13 10:39:54 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const apiRouters = require("./routers/apiRouter");
-
+const cors = require("cors");
 const {
   handlePathError,
   customError,
   handleError404,
 } = require("./errorHandler");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouters);
 app.all("/*", handlePathError);
